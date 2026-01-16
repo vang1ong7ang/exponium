4734d41fc1e7b30d82fb99f2e5c2e63f13853915
vang1ong7ang <vang1ong7ang@0xs-MacBook-Pro.local>
2026-01-16 18:24:00 +0800

    dev

--- src/code.rs
+++ src/code.rs
@@ -5,6 +5,12 @@ pub enum Code {
     Set { dst: usize, val: Float },
     Add { dst: usize, x: usize, y: usize },
     Mul { dst: usize, x: usize, y: usize },
+    Lt { dst: usize, x: usize, y: usize },
+    Le { dst: usize, x: usize, y: usize },
+    Gt { dst: usize, x: usize, y: usize },
+    Ge { dst: usize, x: usize, y: usize },
+    Eq { dst: usize, x: usize, y: usize },
+    Ne { dst: usize, x: usize, y: usize },
     Sqrt { dst: usize, x: usize },
 }
 
@@ -15,6 +21,12 @@ impl Code {
             Code::Set { dst, val } => mem[*dst] = Float::with_val(prec, val),
             Code::Add { dst, x, y } => mem[*dst] = Float::with_val(prec, &mem[*x] + &mem[*y]),
             Code::Mul { dst, x, y } => mem[*dst] = Float::with_val(prec, &mem[*x] * &mem[*y]),
+            Code::Lt { dst, x, y } => mem[*dst] = Float::with_val(prec, if mem[*x] < mem[*y] { 1 } else { 0 }),
+            Code::Le { dst, x, y } => mem[*dst] = Float::with_val(prec, if mem[*x] <= mem[*y] { 1 } else { 0 }),
+            Code::Gt { dst, x, y } => mem[*dst] = Float::with_val(prec, if mem[*x] > mem[*y] { 1 } else { 0 }),
+            Code::Ge { dst, x, y } => mem[*dst] = Float::with_val(prec, if mem[*x] >= mem[*y] { 1 } else { 0 }),
+            Code::Eq { dst, x, y } => mem[*dst] = Float::with_val(prec, if mem[*x] == mem[*y] { 1 } else { 0 }),
+            Code::Ne { dst, x, y } => mem[*dst] = Float::with_val(prec, if mem[*x] != mem[*y] { 1 } else { 0 }),
             Code::Sqrt { dst, x } => mem[*dst] = Float::with_val(prec, mem[*x].sqrt_ref()),
         }
     }
