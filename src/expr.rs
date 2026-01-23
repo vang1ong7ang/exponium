729ff1495dc47bde4f70f9024fa71c8f5ced09f4
vang1ong7ang <vang1ong7ang@0xs-MacBook-Pro.local>
2026-01-23 23:05:59 +0800

    dev

--- src/expr.rs
+++ src/expr.rs
@@ -4,53 +4,58 @@ use rug::ops::Pow;
 use std::collections::HashMap;
 #[derive(Debug, Clone)]
 pub struct Expr {
-    code: Vec<(String, String, String, String)>,
+    code: Vec<(char, char, char, char)>,
 }
 impl Expr {
-    pub fn term(&self, prec: u32, rate: Float, cost: Float, term: Float) -> Option<Float> {
-        let mut game = Game::new(prec, rate, cost);
-        while game.prin() < &term {
-            game.harv(self.eval(game.prin().clone(), game.rate().clone(), game.cost().clone())?);
-        }
-        Some(game.time().clone())
+    pub fn term<const N: usize>(&self, prec: u32, rate: Float, cost: Float, term: Float) -> Option<Float> {
+        let game = (0..N).fold(Some(Game::new(prec, rate, cost)), |game, _| {
+            if let Some(mut game) = game {
+                if game.prin() < &term {
+                    game.harv(self.eval(game.prin().clone(), game.rate().clone(), game.cost().clone())?)
+                }
+                Some(game)
+            } else {
+                None
+            }
+        })?;
+        if game.prin() < &term { None } else { Some(game.time().clone()) }
     }
     pub fn eval(&self, prin: Float, rate: Float, cost: Float) -> Option<Float> {
         let mut mem = HashMap::new();
-        mem.insert("prin", prin);
-        mem.insert("rate", rate);
-        mem.insert("cost", cost);
+        mem.insert('P', prin);
+        mem.insert('R', rate);
+        mem.insert('C', cost);
         for (z, o, x, y) in &self.code {
-            let x = mem.get(x.as_str())?.clone();
-            let y = mem.get(y.as_str())?.clone();
-            let z = z.as_str();
+            let x = mem.get(x)?.clone();
+            let y = mem.get(y)?.clone();
             if mem.contains_key(z) {
                 return None;
             }
-            match o.as_str() {
-                "add" => {
-                    mem.insert(z, x + y);
+            match o {
+                '+' => {
+                    mem.insert(*z, x + y);
                 }
-                "mul" => {
-                    mem.insert(z, x * y);
+                '*' => {
+                    mem.insert(*z, x * y);
                 }
-                "log" => {
-                    mem.insert(z, x.ln() / y.ln());
+                'v' => {
+                    mem.insert(*z, y.ln() / x.ln());
                 }
-                "pow" => {
-                    mem.insert(z, x.pow(y));
+                '^' => {
+                    mem.insert(*z, x.pow(y));
                 }
                 _ => return None,
             }
         }
-        mem.get("wait").cloned()
+        mem.get(&'T').cloned()
     }
 }
-impl From<Vec<(String, String, String, String)>> for Expr {
-    fn from(code: Vec<(String, String, String, String)>) -> Self {
+impl From<Vec<(char, char, char, char)>> for Expr {
+    fn from(code: Vec<(char, char, char, char)>) -> Self {
         Expr { code }
     }
 }
-impl From<Expr> for Vec<(String, String, String, String)> {
+impl From<Expr> for Vec<(char, char, char, char)> {
     fn from(expr: Expr) -> Self {
         expr.code
     }
